001c00176aa0afe056d78c48d75aefb8dfae0be8
Andreas von Studnitz <avs@integer-net.de>
2014-07-06 23:02:56 +0200

    Added Config Unit Tests

---
+++ src/app/code/community/IntegerNet/Solr/Test/Config/Config.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * integer_net Magento Module
+ *
+ * @category   IntegerNet
+ * @package    IntegerNet_Solr
+ * @copyright  Copyright (c) 2014 integer_net GmbH (http://www.integer-net.de/)
+ * @author     Andreas von Studnitz <avs@integer-net.de>
+ */
+class IntegerNet_Solr_Test_Config_Config extends EcomDev_PHPUnit_Test_Case_Config
+{
+    /**
+     * @test
+     * @loadExpections
+     */
+    public function globalConfig()
+    {
+        $this->assertModuleVersionGreaterThanOrEquals('0.1.0');
+        $this->assertModuleCodePool('community');
+    }
+
+    /**
+     * @test
+     */
+    public function modelConfig()
+    {
+        $this->assertModelAlias('integernet_solr/indexer', 'IntegerNet_Solr_Model_Indexer');
+        $this->assertModelAlias('integernet_solr/resource_indexer', 'IntegerNet_Solr_Model_Resource_Indexer');
+    }
+
+    /**
+     * @test
+     */
+    public function helperConfig()
+    {
+        $this->assertHelperAlias('integernet_solr', 'IntegerNet_Solr_Helper_Data');
+    }
+
+    /**
+     * @test
+     */
+    public function translationConfig()
+    {
+        $this->assertConfigNodeValue('adminhtml/translate/modules/integernet_solr/files/default', 'IntegerNet_Solr.csv');
+    }
+
+    /**
+     * @test
+     */
+    public function indexerConfig()
+    {
+        $this->assertConfigNodeValue('global/index/indexer/integernet_solr/model', 'integernet_solr/indexer');
+    }
+}

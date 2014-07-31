9de39591d3972ca8d6325c648de725e84aa05aae
Andreas von Studnitz <avs@integer-net.de>
2014-07-31 22:12:16 +0200

    Retrieve filterable attributes

--- src/app/code/community/IntegerNet/Solr/Model/Indexer/Product.php
+++ src/app/code/community/IntegerNet/Solr/Model/Indexer/Product.php
@@ -10,7 +10,10 @@
 class IntegerNet_Solr_Model_Indexer_Product extends Mage_Core_Model_Abstract
 {
     /** @var Mage_Catalog_Model_Entity_Attribute[] */
-    protected $_attributes = array();
+    protected $_searchableAttributes = null;
+
+    /** @var Mage_Catalog_Model_Entity_Attribute[] */
+    protected $_filterableInSearchAttributes = null;
 
     protected $_resourceName = 'integernet_solr/indexer';
 
@@ -33,15 +36,17 @@ class IntegerNet_Solr_Model_Indexer_Product extends Mage_Core_Model_Abstract
             $productCollection = Mage::getResourceModel('catalog/product_collection')
                 ->setStoreId($storeId)
                 ->addUrlRewrite()
+                ->addAttributeToFilter('vibility', array('in' => Mage::getSingleton('catalog/product_visibility')->getVisibleInSearchIds()))
+                ->addAttributeToFilter('status', Mage_Catalog_Model_Product_Status::STATUS_ENABLED)
                 ->addAttributeToSelect(array('visibility', 'status'))
-                ->addAttributeToSelect($this->_getSearchableAttributeCodes());
+                ->addAttributeToSelect($this->_getSearchableAttributes()->getColumnValues('attribute_code'))
+                ->addAttributeToSelect($this->_getFilterableInSearchAttributes()->getColumnValues('attribute_code'));
 
             if (is_array($productIds)) {
                 $productCollection->addAttributeToFilter('entity_id', array('in' => $productIds));
             }
 
             $combinedProductData = array();
-            $productSkusForDeletion = array();
 
             foreach($productCollection as $product) {
                 /** @var Mage_Catalog_Model_Product $product */
@@ -49,17 +54,11 @@ class IntegerNet_Solr_Model_Indexer_Product extends Mage_Core_Model_Abstract
                 $productData = $this->_getProductData($product);
                 if (is_array($productData)) {
                     $combinedProductData[] = $productData;
-                } else {
-                    $productSkusForDeletion[] = $product->getSku();
                 }
             }
 
             if ($emptyIndex) {
                 $this->getResource()->deleteAllDocuments($storeId);
-            } else {
-                if (sizeof($productSkusForDeletion)) {
-                    $this->getResource()->deleteByMultipleIds($storeId, $productSkusForDeletion);
-                }
             }
 
             if (sizeof($combinedProductData)) {
@@ -118,79 +117,36 @@ class IntegerNet_Solr_Model_Indexer_Product extends Mage_Core_Model_Abstract
      */
     protected function _canIndexProduct($product)
     {
-        if (!$product->isVisibleInCatalog()) {
-            return false;
-        }
-
         return true;
     }
 
     /**
-     * @return string[]
+     * @return Mage_Catalog_Model_Entity_Attribute[]
      */
-    protected function _getSearchableAttributeCodes()
+    protected function _getSearchableAttributes()
     {
-        $attributeCodes = array(
-            'name',
-            'description',
-            'short_description',
-        );
+        if (is_null($this->_searchableAttributes)) {
 
-        return $attributeCodes;
+            /** @var $attributes Mage_Catalog_Model_Resource_Product_Attribute_Collection */
+            $this->_searchableAttributes = Mage::getResourceModel('catalog/product_attribute_collection')
+                ->addIsSearchableFilter();
+        }
+
+        return $this->_searchableAttributes;
     }
 
     /**
-     * @param Mage_Catalog_Model_Product $product
-     * @param string $attributeCode
-     * @return string
+     * @return Mage_Catalog_Model_Entity_Attribute[]
      */
-    protected function _getRawAttributeValue(Mage_Catalog_Model_Product $product, $attributeCode)
+    protected function _getFilterableInSearchAttributes()
     {
-        /** @var $attribute Mage_Catalog_Model_Resource_Eav_Attribute */
-        $attribute = $this->_getAttribute($attributeCode);
-        if (!$attribute instanceof Mage_Catalog_Model_Resource_Eav_Attribute) {
-            return null;
+        if (is_null($this->_filterableInSearchAttributes)) {
+
+            /** @var $attributes Mage_Catalog_Model_Resource_Product_Attribute_Collection */
+            $this->_filterableInSearchAttributes = Mage::getResourceModel('catalog/product_attribute_collection')
+                ->addIsFilterableInSearchFilter();
         }
 
-        switch($attribute->getFrontendInput()) {
-            case 'textarea':
-                return str_replace(array("\r\n", "\n", "\r"), '<br />', trim($product->getData($attributeCode)));
-            case 'select':
-                return $product->getAttributeText($attributeCode);
-            case 'price':
-                return $this->_formatPrice($product->getData($attributeCode), 2, '.', '');
-            case 'date':
-                $date = new Zend_Date($product->getData($attributeCode), Zend_Date::ISO_8601);
-                $date->setTimezone(Mage::getStoreConfig('general/locale/timezone'));
-                return $date->get(Zend_Date::ISO_8601);
-            default:
-                if (is_array($product->getData($attributeCode))) {
-                    return implode(', ', $product->getData($attributeCode));
-                }
-                return trim($product->getData($attributeCode));
-        }
-    }
-
-    /**
-     * @param string $attributeCode
-     * @return Mage_Catalog_Model_Resource_Eav_Attribute
-     */
-    protected function _getAttribute($attributeCode)
-    {
-        if (!isset($this->_attributes[$attributeCode])) {
-            $this->_attributes[$attributeCode] = Mage::getSingleton('eav/config')
-                ->getAttribute('catalog_product', $attributeCode);
-        }
-
-        return $this->_attributes[$attributeCode];
-    }
-
-    /**
-     * @param float $price
-     * @return string
-     */
-    protected function _formatPrice($price)
-    {
-        return number_format($price, 2, '.', '');
+        return $this->_filterableInSearchAttributes;
     }
 }

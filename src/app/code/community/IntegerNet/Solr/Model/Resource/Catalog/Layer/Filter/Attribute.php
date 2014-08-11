9414949f1cd37fe263e9f7d07fbe521ce14b1a48
Andreas von Studnitz <avs@integer-net.de>
2014-08-11 22:07:52 +0200

    Substitute attribute filter counts with solr result values

---
+++ src/app/code/community/IntegerNet/Solr/Model/Resource/Catalog/Layer/Filter/Attribute.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * integer_net Magento Module
+ *
+ * @category   IntegerNet
+ * @package    IntegerNet_Solr
+ * @copyright  Copyright (c) 2014 integer_net GmbH (http://www.integer-net.de/)
+ * @author     Andreas von Studnitz <avs@integer-net.de>
+ */ 
+class IntegerNet_Solr_Model_Resource_Catalog_Layer_Filter_Attribute extends Mage_Catalog_Model_Resource_Layer_Filter_Attribute
+{
+    /**
+     * Retrieve array with products counts per attribute option
+     *
+     * @param Mage_Catalog_Model_Layer_Filter_Attribute $filter
+     * @return array
+     */
+    public function getCount($filter)
+    {
+        /** @var $solrResult StdClass */
+        $solrResult = Mage::getSingleton('integernet_solr/result')->getSolrResult();
+
+        $attribute  = $filter->getAttributeModel();
+
+        $count = array();
+        if (isset($solrResult->facet_counts->facet_fields->{$attribute->getAttributeCode() . '_facet'})) {
+            foreach((array)$solrResult->facet_counts->facet_fields->{$attribute->getAttributeCode() . '_facet'} as $key => $value) {
+                $count[intval($key)] = $value;
+            }
+            return $count;
+        }
+
+        return array();
+    }
+}

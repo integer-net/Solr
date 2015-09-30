dc8ae52f690f2ca699851938a41ee8cea34d62f4
Andreas von Studnitz <avs@integer-net.de>
2015-09-30 11:26:11 +0200

    Add button to clear / recreate cache in backend

---
+++ src/app/code/community/IntegerNet/Solr/controllers/Adminhtml/SolrController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * integer_net Magento Module
+ *
+ * @category   IntegerNet
+ * @package    IntegerNet_Solr
+ * @copyright  Copyright (c) 2015 integer_net GmbH (http://www.integer-net.de/)
+ * @author     Andreas von Studnitz <avs@integer-net.de>
+ */
+class IntegerNet_Solr_Adminhtml_SolrController extends Mage_Adminhtml_Controller_Action
+{
+    public function flushAction()
+    {
+        Mage::getSingleton('adminhtml/session')->addSuccess(
+            $this->__('The Solr Autosuggest Cache has been flushed and rebuilt successfully.')
+        );
+        Mage::helper('integernet_solr/autosuggest')->storeSolrConfig();
+        $this->_redirectReferer();
+    }
+}

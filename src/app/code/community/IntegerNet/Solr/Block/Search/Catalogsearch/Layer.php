5ec090717b83a5d32df28e704a9fefb098494b97
Milanowicz <amaru-jive@gmx.de>
2015-10-21 15:55:09 +0200

    Get a Singleton and not new Class.

--- src/app/code/community/IntegerNet/Solr/Block/Search/Catalogsearch/Layer.php
+++ src/app/code/community/IntegerNet/Solr/Block/Search/Catalogsearch/Layer.php
@@ -1,17 +1,19 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: mhacker
- * Date: 20.10.15
- * Time: 10:24
- */ 
+ * integer_net Magento Module
+ *
+ * @category   IntegerNet
+ * @package    IntegerNet_Solr
+ * @copyright  Copyright (c) 2015 integer_net GmbH (http://www.integer-net.de/)
+ * @author     Milan Hacker
+ */
 class IntegerNet_Solr_Block_Search_Catalogsearch_Layer extends Enterprise_Search_Block_Catalogsearch_Layer {
 
     /**
      * @return bool
      */
     public function canShowBlock () {
-        $solrResult = Mage::getModel('integernet_solr/result')->getSolrResult();
+        $solrResult = Mage::getSingleton('integernet_solr/result')->getSolrResult();
         return (sizeof($solrResult->response->docs) > 0) ? true : false;
     }
 }

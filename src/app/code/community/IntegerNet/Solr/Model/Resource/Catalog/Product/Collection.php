8ce47e3d5ec0e227f4203de2f2e2e7e8e4b18f44
Andreas von Studnitz <avs@integer-net.de>
2015-10-28 10:02:33 +0100

    Add compatibility to enabled flat indexes

--- src/app/code/community/IntegerNet/Solr/Model/Resource/Catalog/Product/Collection.php
+++ src/app/code/community/IntegerNet/Solr/Model/Resource/Catalog/Product/Collection.php
@@ -34,7 +34,7 @@ class IntegerNet_Solr_Model_Resource_Catalog_Product_Collection extends Mage_Cat
         }
         return $this->_solrResultCollection;
     }
-    
+
     protected function _beforeLoad()
     {
         if (Mage::helper('integernet_solr')->isActive() && is_null($this->_solrResultCollection)) {
@@ -46,7 +46,7 @@ class IntegerNet_Solr_Model_Resource_Catalog_Product_Collection extends Mage_Cat
 
     /**
      * Bring collection items into order from solr
-     * 
+     *
      * @return IntegerNet_Solr_Model_Resource_Catalog_Product_Collection
      */
     protected function _afterLoad()
@@ -58,20 +58,20 @@ class IntegerNet_Solr_Model_Resource_Catalog_Product_Collection extends Mage_Cat
         parent::_afterLoad();
 
         $tempItems = array();
-        foreach($this->getSolrResultCollection()->getColumnValues('product_id') as $itemId) {
+        foreach ($this->getSolrResultCollection()->getColumnValues('product_id') as $itemId) {
             $item = $this->getItemById($itemId);
             if (!is_null($item)) {
                 $tempItems[$itemId] = $item;
             }
         }
         $this->_items = $tempItems;
-        
+
         return $this;
     }
 
     /**
      * Get Collection size from Solr
-     * 
+     *
      * @return int
      */
     public function getSize()
@@ -80,5 +80,40 @@ class IntegerNet_Solr_Model_Resource_Catalog_Product_Collection extends Mage_Cat
             return parent::getSize();
         }
         return $this->getSolrResultCollection()->getSize();
+    }
+
+    /**
+     * Add attribute to sort order
+     * Fix for search result display with enabled flat index and not html from solr index
+     *
+     * @param string $attribute
+     * @param string $dir
+     * @return Mage_Catalog_Model_Resource_Product_Collection
+     */
+    public function addAttributeToSort($attribute, $dir = self::SORT_ORDER_ASC)
+    {
+        if (!$this->isEnabledFlat()) {
+            return parent::addAttributeToSort($attribute, $dir);
+        }
+
+        if (!Mage::helper('integernet_solr')->isActive()) {
+            return parent::addAttributeToSort($attribute, $dir);
+        }
+
+        if ($attribute == 'position') {
+            if (isset($this->_joinFields[$attribute])) {
+                $this->getSelect()->order($this->_getAttributeFieldName($attribute) . ' ' . $dir);
+                return $this;
+            }
+            // optimize if using cat index
+            $filters = $this->_productLimitationFilters;
+            if (isset($filters['category_id']) || isset($filters['visibility'])) {
+                $this->getSelect()->order('cat_index.position ' . $dir);
+            } else {
+                $this->getSelect()->order('e.entity_id ' . $dir);
+            }
+
+            return $this;
+        }
     }
 }

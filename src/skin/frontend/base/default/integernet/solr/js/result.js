fc246e8239e0d26d9c20eee1e10886c7ca2cb569
Andreas von Studnitz <avs@integer-net.de>
2016-03-18 14:07:16 +0100

    4710 Refactoring JS

--- src/skin/frontend/base/default/integernet/solr/js/result.js
+++ src/skin/frontend/base/default/integernet/solr/js/result.js
@@ -1,38 +1,64 @@
-var isFirstCall = true;
+var SolrResult = Class.create();
+SolrResult.prototype = {
 
-var updateLinks = function () {
-    if (isFirstCall) {
-        var links = $$('.block-layered-nav a', '.toolbar a', '.toolbar-bottom a');
-        isFirstCall = false;
-    } else {
-        links = $$('.toolbar a', '.toolbar-bottom a');
-    }
+    isFirstCall: true,
+    
+    initialize: function() {
+        this.updateLinks();
+    },
 
-    links.each(function (element) {
-        element.observe('click', function (e) {
-            Event.stop(e);
-            var url = element.href;
-
-            var contentElement = $$('.col-main')[0];
-
-            new Ajax.Updater(contentElement, url, {
-                insertion: 'bottom',
-                onSuccess: function(response) {
-                    var lastChild = $$(".col-main > :last-child");
-                    while (!lastChild[0].hasClassName('block-layered-nav')) {
-                        lastChild[0].remove();
-                        lastChild = $$(".col-main > :last-child");
-                    }
-                    console.log('calling');
-                },
-                onComplete: function (response) {
-                    updateLinks();
+    updateResults: function (url) {
+        var self = this;
+        var contentElement = $$('.col-main')[0];
+        new Ajax.Updater(contentElement, url, {
+            insertion: 'bottom',
+            onSuccess: function (response) {
+                var lastChild = $$(".col-main > :last-child");
+                while (!lastChild[0].hasClassName('block-layered-nav')) {
+                    lastChild[0].remove();
+                    lastChild = $$(".col-main > :last-child");
                 }
+            },
+            onComplete: function (response) {
+                self.updateLinks();
+            }
+        });
+    }, 
+    
+    updateLinks: function () {
+        var self = this;
+        var links;
+        if (this.isFirstCall) {
+            links = $$('.block-layered-nav a', '.toolbar a', '.toolbar-bottom a');
+            this.isFirstCall = false;
+        } else {
+            links = $$('.toolbar a', '.toolbar-bottom a');
+        }
+
+        links.each(function (element) {
+            element.observe('click', function (e) {
+                Event.stop(e);
+                var url = element.href;
+
+                self.updateResults(url);
             });
         });
-    });
+
+        var dropdowns = $$('.toolbar select', '.toolbar-bottom select');
+
+        dropdowns.each(function (element) {
+            element.onchange = undefined;
+            element.observe('change', function (e) {
+                
+                var url = element.value;
+
+                self.updateResults(url);
+            });
+        });
+    }
 };
 
+
 document.observe("dom:loaded", function() {
-    updateLinks();
+    var solrResult = new SolrResult();
 });

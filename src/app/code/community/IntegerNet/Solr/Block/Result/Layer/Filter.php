ddc6d51f4e48e1b30fc04b93d4b53345277f4e73
LimeSoda Office <office@limesoda.com>
2016-05-18 12:45:37 +0100

    Sort filter alphabetically (#31392)

--- src/app/code/community/IntegerNet/Solr/Block/Result/Layer/Filter.php
+++ src/app/code/community/IntegerNet/Solr/Block/Result/Layer/Filter.php
@@ -58,7 +58,10 @@ class IntegerNet_Solr_Block_Result_Layer_Filter extends Mage_Core_Block_Template
             return $this->_getRangeFilterItems();
         }
 
-        return $this->_getAttributeFilterItems();
+        $items = $this->_getAttributeFilterItems();
+        usort($items, array($this, "sortItems"));
+
+        return $items;
     }
 
     /**
@@ -314,7 +317,8 @@ class IntegerNet_Solr_Block_Result_Layer_Filter extends Mage_Core_Block_Template
                 $item->setIsChecked($this->_isSelected($attributeCode, $optionId));
                 $item->setType('attribute');
                 $item->setOptionId($optionId);
-                
+                $item->setText($this->getAttribute()->getSource()->getOptionText($optionId));
+
                 Mage::dispatchEvent('integernet_solr_filter_item_create', array(
                     'item' => $item,
                     'solr_result' => $this->_getSolrResult(),
@@ -420,5 +424,14 @@ class IntegerNet_Solr_Block_Result_Layer_Filter extends Mage_Core_Block_Template
     protected function _getRoute()
     {
         return 'catalogsearch/result/*';
+    }
+
+    /**
+     * @param $a Varien_Object
+     * @param $b Varien_Object
+     * @return Varien_Object[]
+     */
+    protected function sortItems($a, $b) {
+        return strcmp($a->getData('text'), $b->getData('text'));
     }
 }

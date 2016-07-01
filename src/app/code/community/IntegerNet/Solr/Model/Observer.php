a93fab5f54cee9b7143b1af599e97638d2167aa9
Andreas von Studnitz <avs@integer-net.de>
2016-07-01 11:07:22 +0200

    Don't redirect to product from different website or to disabled product

--- src/app/code/community/IntegerNet/Solr/Model/Observer.php
+++ src/app/code/community/IntegerNet/Solr/Model/Observer.php
@@ -235,7 +235,10 @@ class IntegerNet_Solr_Model_Observer
             $product = Mage::getModel('catalog/product');
             if ($productId = $product->getIdBySku($query)) {
                 $product->load($productId);
-                if ($product->isVisibleInSiteVisibility()) {
+                if ($product->isVisibleInSiteVisibility()
+                    && $product->isAvailable()
+                    && in_array(Mage::app()->getWebsite(), $product->getWebsiteIds())
+                ) {
                     return $product->getProductUrl();
                 }
             }
@@ -258,6 +261,7 @@ class IntegerNet_Solr_Model_Observer
         $matchingProductCollection = Mage::getResourceModel('catalog/product_collection');
         $matchingProductCollection
             ->addStoreFilter()
+            ->addWebsiteFilter()
             ->addAttributeToFilter($filters)
             ->addAttributeToFilter('visibility', array('in' => Mage::getSingleton('catalog/product_visibility')->getVisibleInSearchIds()))
             ->addAttributeToSelect('url_key');
